8da969b0bc11bfdef0d72f433fa36f980d9f807d
unknown <ag.prog.full@outlook.com>
2023-05-06 19:35:15 -0300

    prueba id UUID

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,13 +4,14 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
-    // id: {
-    //   type: DataTypes.INTEGER,
-    //   //type: DataTypes.UUID,
-    //   //defaultValue: DataTypes.UUIDV4,
-    //   allowNull: false,
-    //   primaryKey: true,
-    // },
+    id: {
+      // type: DataTypes.INTEGER,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
