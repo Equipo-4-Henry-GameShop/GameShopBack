f754d2ab6033db56931187a5d592130fbe8ef131
unknown <ag.prog.full@outlook.com>
2023-05-24 01:46:16 -0300

    create route login

--- api/src/routes/login.js
+++ api/src/routes/login.js
@@ -7,17 +7,17 @@ router.use(express.json());
 
 
 // Ruta para iniciar sesión
-router.get('/', async (req, res) => {
+router.post('/', async (req, res) => {
     
     let { user, password } = req.body;
     try {
 
       let dataUser = await login(user, password)
+      console.log(dataUser);
 
     // Enviar el los datos al frontend
     res.status(200).json( dataUser );
   } catch (error) {
-    console.error(error);
     res.status(500).json({ message: 'Error interno del servidor' });
   }
 });
